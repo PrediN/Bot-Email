9b31f158ca41d062083eb724213ea00449bfb91c
Pedro Sanchez <64911151+PrediN@users.noreply.github.com>
2025-02-19 13:38:59 -0300

    Criação de um arquivo de texto toda vez que executar o arquivo

--- main/search.py
+++ main/search.py
@@ -1,3 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
+r = requests.get("https://duckduckgo.com/?q=Inteligência+artificial+avanços&atb=v458-1&df=w&ia=web")
+
+soup = BeautifulSoup(r.text, 'html.parser')
+
+print(soup.prettify())
+
+print("Diretório de trabalho:", os.getcwd())
+
+# Extração para arquivo em txt
+
+with open('output.txt', 'w', encoding='utf-8') as file:
+    file.write(soup.prettify())
+    print("Arquivo criado com sucesso")
